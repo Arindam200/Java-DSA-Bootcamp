7f8899441983bb262a799086f4ecbee1b2ac1bc8
Arindam Majumder <109217591+Arindam200@users.noreply.github.com>
2022-09-21 01:07:59 +0530

    Q-02 added

---
+++ 02-first-java/Q_02.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Q_02 {
+    public static void main(String[] args) {
+        System.out.print("Enter Your Name: ");
+        Scanner input = new Scanner(System.in);
+            String S = input.next();
+        System.out.println("Hello! "+ S + " ,Nice to meet you");
+    }
+}
