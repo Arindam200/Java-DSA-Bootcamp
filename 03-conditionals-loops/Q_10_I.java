17ac6534685531bc1956a1f1b73a37ce344f09cb
Arindam Majumder <109217591+Arindam200@users.noreply.github.com>
2022-09-28 00:29:23 +0530

    Add files via upload

---
+++ 03-conditionals-loops/Q_10_I.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+
+public class Q_10_I {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the Percentage: ");
+         int p = in.nextInt();
+
+        double CGPA =  (p / 9.5);
+
+
+        System.out.print("The CGPA is "+CGPA);
+
+
+
+
+
+
+    }
+}
